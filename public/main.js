ea6fd0d120e5539705b69a6cd1ea07a685d2b085
shushu <907896638@qq.com>
2017-05-27 11:50:33 +0800

    add title icon

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 var userLogin = true;
+var TYPING_TIMER_LENGTH = 800; // ms
 var socket = io();
 var userName;
+var lastTyingTime;
 
 var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
@@ -10,15 +12,40 @@ var COLORS = [
 
 var index = Math.ceil(Math.random()*COLORS.length);
 
+let connected = false;
+let typing = false;
+
+
 window.addEventListener('keydown', function(event) {
+	if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+    	document.querySelector('.input-message').focus();
+    }
+
 	if(event.keyCode == 13) {
 		if(userLogin) {
+			connected = true;
 			log("Welcome to Chat");
 			changeToChat();
 		}
 		else {
 			userMessaage();
+			socket.emit("stop typing");
 		}	
+	}
+	else {
+		if(connected && !typing) {
+			typing = true;
+			socket.emit('is typing');
+		}
+
+		lastTyingTime = new Date().getTime();
+		setTimeout(() => {
+			var nowTime = new Date().getTime();
+			if((nowTime - lastTyingTime > TYPING_TIMER_LENGTH) && typing) {
+				typing = false;
+				socket.emit("stop typing");
+			}
+		}, TYPING_TIMER_LENGTH);
 	}
 });
 
@@ -41,6 +68,14 @@ function addMessage(value, index) {
 	let log = document.createElement('li');
 	log.innerHTML = "<span class='username' style='color: " + COLORS[index] + "'>" + userName + "</span><span>" + value + "</span>";
 	log.classList.add('message');
+	document.querySelector('.messages').appendChild(log);
+}
+
+function addTyping(username) {
+	let log = document.createElement('li');
+	log.classList.add("typing");
+	log.dataset.username = username;
+	log.innerHTML = username + " is typing";
 	document.querySelector('.messages').appendChild(log);
 }
 
@@ -71,6 +106,27 @@ socket.on('user left', (data) => {
 
 socket.on('add message', (data) => {
 	addMessage(data.message, data.color);
+});
+
+socket.on('connect_error', () => {
+	log("attempt to reconnect has failed");
+});
+
+socket.on('is typing', (message) => {
+	addTyping(message.username);
+});
+
+socket.on('stop typing', (username) => {
+	let messages = document.querySelector('.messages');
+	let logMessage = document.querySelectorAll('.typing');
+	logMessage.forEach((element) => {
+		if(element.dataset.username == username) {
+			element.classList.add('delete');
+			setTimeout(() => {
+				messages.removeChild(element);
+			}, 600);
+		} 
+	});	
 });
 
 
